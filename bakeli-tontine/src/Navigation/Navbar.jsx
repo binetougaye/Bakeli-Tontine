b2acb7b1fb82ec60b44c12825deb8456a40cf564
Khadija <dija5631@gmail.com>
2024-05-08 22:18:49 +0200

    second

--- bakeli-tontine/src/Navigation/Navbar.jsx
+++ bakeli-tontine/src/Navigation/Navbar.jsx
@@ -1,63 +1,34 @@
 import React from 'react';
-import  './Navigation.css';
+import './Navigation.css'
+import images from './imagess.png';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { RxAvatar } from "react-icons/rx";
 import { GrNotification } from "react-icons/gr";
-
-const items = [
-  {
-    key: '1',
-    label: (
-      <a href="#">
-        Profil
-      </a>
-    ),
-  },
-  {
-    key: '2',
-    label: (
-      <a href="#">
-        Modifier le Mot de Passe
-      </a>
-    ),
-  },
-  {
-    key: '3',
-    label: (
-      <a href="#">
-        Déconnexion
-      </a>
-    ),
-  },
-];
 
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container">
-        <a className="navbar-brand para" href="#">Paramètres</a>
+        <h1 className="navbar-brand para" href="#">Paramètres</h1>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0 position-absolute end-0">
             <li className="nav-item">
               <GrNotification className='nav-ico'/>
             </li>
             <li className="nav-item">
-              <RxAvatar className='nav-icons'/>
+              <img  className="image"src={images}alt="imagess" />
             </li>
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-               <strong>Ndiaga SALL </strong><br />
+            <li className="dropdown" data-bs-theme="light">
+               <strong className="dropdown-toggle" type="text" id="dropdownMenuButtonLight" data-bs-toggle="dropdown" aria-expanded="false">Ndiaga SALL </strong><br />
                <span>Administrator</span>
-              </a>
-              <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                {items.map(item => (
-                  <li key={item.key}>{item.label}</li>
-                ))}
-              </ul>
+  <ul className="dropdown-menu text-center" aria-labelledby="dropdownMenuButtonLight">
+    <li><a className="dropdown-item" href="#">Profil</a></li>
+    <li><a className="dropdown-item" href="#"> Modifier le Mot de Passe</a></li>
+    <li><a className="dropdown-item" href="#">Déconnexion</a></li>    
+  </ul>
             </li>
           </ul>
         </div>
--- bakeli-tontine/src/Navigation/Sidebar.jsx
+++ bakeli-tontine/src/Navigation/Sidebar.jsx
@@ -1,56 +1,58 @@
-import React from 'react';
-import  './Navigation.css';
-import { FaBriefcase } from "react-icons/fa6";
-import { Dropdown } from 'antd';
+import React from "react";
+import './Navigation.css'
 import { RxDashboard } from "react-icons/rx";
 import { FiUser } from "react-icons/fi";
-import { GiPayMoney } from "react-icons/gi";
+import health from './healthicons_money-bag-outline.svg';
+import vector from './fontisto_wallet.png';
 import { IoSettingsOutline } from "react-icons/io5";
-import { IoIosArrowDown } from "react-icons/io";
-import { Space } from 'antd';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-const items = [
-    {
-      label: <a href="#">Paramètres Généraux</a>,
-      key: '0',
-    },
-    {
-      label: <a href="#">Utilisateurs</a>,
-      key: '1',
-    },
-    {
-        label: <a href="#">Archives</a>,
-        key: '2',
-      },
-      {
-        label: <a href="#">Membres Bloqués</a>,
-        key: '3',
-      },
-  ];
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
 function Sidebar() {
-    return (
-      <aside className="sidebar">
-        <div className='sidebar-top'>
-        <h3 className='hach'><FaBriefcase />&nbsp;&nbsp;Bakéli Tontine</h3>
+  return (
+    <aside className="sidebar">
+      <div className="sidebar-top">
+        <h3 className="hach">
+          <img src={vector} alt="" />
+          &nbsp;&nbsp;Bakéli Tontine
+        </h3>
         <ul>
-          <li><a href="#"> <RxDashboard className='nav-icon'/>Dashboard</a></li>
-          <li><a href="#"><FiUser className='nav-icon' />Utilisateurs</a></li>
-          <li><a href="#"> <GiPayMoney className='nav-icon'/>Cotisations</a></li>
           <li>
-          <Dropdown menu={{ items}}trigger={['click']}>
-           <a onClick={(e) => e.preventDefault()}>
-        <Space>
-        <IoSettingsOutline className='nav-icon' />
-         Paramètres
-       <IoIosArrowDown className='tap' />
-        </Space>
-      </a>
-    </Dropdown>
+            <a href="#">
+              {" "}
+              <RxDashboard className="nav-icon" />
+              Dashboard
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <FiUser className="nav-icon" />
+              Utilisateurs
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              {" "}
+              <img src={health} alt=""  className="nav-icon"/>
+              Cotisations
+            </a>
+          </li>
+
+          <li className="setting">
+            <IoSettingsOutline className="nav-ican" />
+            <select id="custom-dropdown" className="dropdowns text-center " >
+              <option disabled selected hidden>
+                <a href="">Paramètres</a>
+              </option>
+              <option value="1">Paramètres Généraux</option>
+              <option value="2">Utilisateurs</option>
+              <option value="3">Archives</option>
+              <option value="4">Membres Bloquès</option>
+            </select>
           </li>
         </ul>
-        </div>
-      </aside>
-    );
-  }
-  export default Sidebar;
+      </div>
+    </aside>
+  );
+}
+export default Sidebar;
